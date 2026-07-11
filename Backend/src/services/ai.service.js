cbc7eb5f2e18cc47ea562b68fe17cc54914fca39
madhav <madhavjoshi6280@gmail.com>
2026-07-11 20:11:27 +0530

    changed gemini model

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -109,7 +109,7 @@ ${jobDescription}
 `;
 
     const response = await ai.models.generateContent({
-        model: "gemini-2.5-flash",
+        model: "gemini-3-flash-preview",
         contents: prompt,
         config: {
             responseMimeType: "application/json"
