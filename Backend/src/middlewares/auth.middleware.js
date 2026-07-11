e70cd8a7a1834a9086944d1630d303b206ee9095
madhav <madhavjoshi6280@gmail.com>
2026-07-11 19:24:04 +0530

    9

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -1,3 +1,43 @@
+// const jwt = require("jsonwebtoken");
+// const blacklistTokenModel = require("../models/blacklist.model");
+
+// const authUser = async (req, res, next) => {
+//     try {
+//         console.log("Cookies:", req.cookies);
+
+//         const token = req.cookies?.token;
+
+//         if (!token) {
+//             return res.status(401).json({
+//                 message: "Token not provided"
+//             });
+//         }
+
+//         const blacklisted = await blacklistTokenModel.findOne({ token });
+
+//         if (blacklisted) {
+//             return res.status(401).json({
+//                 message: "Token is invalid"
+//             });
+//         }
+
+//         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+//         req.user = decoded;
+
+//         next();
+
+//     } catch (err) {
+//         console.log(err);
+
+//         return res.status(401).json({
+//             message: "Invalid Token"
+//         });
+//     }
+// };
+
+// module.exports = { authUser };
+
 const jwt = require("jsonwebtoken");
 const blacklistTokenModel = require("../models/blacklist.model");
 
@@ -6,32 +46,36 @@ const authUser = async (req, res, next) => {
         console.log("Cookies:", req.cookies);
 
         const token = req.cookies?.token;
+        console.log("Token:", token);
 
         if (!token) {
+            console.log("❌ No token found");
             return res.status(401).json({
                 message: "Token not provided"
             });
         }
 
         const blacklisted = await blacklistTokenModel.findOne({ token });
+        console.log("Blacklisted:", blacklisted);
 
         if (blacklisted) {
+            console.log("❌ Token is blacklisted");
             return res.status(401).json({
                 message: "Token is invalid"
             });
         }
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        console.log("✅ Decoded:", decoded);
 
         req.user = decoded;
-
         next();
 
     } catch (err) {
-        console.log(err);
-
+        console.error("JWT Error:", err);
         return res.status(401).json({
-            message: "Invalid Token"
+            message: "Invalid Token",
+            error: err.message
         });
     }
 };
