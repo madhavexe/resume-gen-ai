929d88cf1885718bae727a3fd4ccef16ab8e9e05
madhav <madhavjoshi6280@gmail.com>
2026-07-14 12:15:06 +0530

    chnaged to groq

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -1,54 +1,137 @@
-const { GoogleGenAI } = require("@google/genai");
-const { z } = require("zod");
-const {zodToJsonSchema} = require('zod-to-json-schema')
+// const { GoogleGenAI } = require("@google/genai");
+// const { z } = require("zod");
+// const {zodToJsonSchema} = require('zod-to-json-schema')
 
-const ai = new GoogleGenAI({
-    apiKey: process.env.GOOGLE_GENAI_API_KEY
-});
+// const ai = new GoogleGenAI({
+//     apiKey: process.env.GOOGLE_GENAI_API_KEY
+// });
 
-const interviewReportSchema = z.object({
-    matchScore: z.number(),
-    title: z.string(),
+// const interviewReportSchema = z.object({
+//     matchScore: z.number(),
+//     title: z.string(),
 
-    technicalQuestions: z.array(
-        z.object({
-            question: z.string(),
-            intention: z.string(),
-            answer: z.string()
-        })
-    ),
+//     technicalQuestions: z.array(
+//         z.object({
+//             question: z.string(),
+//             intention: z.string(),
+//             answer: z.string()
+//         })
+//     ),
 
-    behavioralQuestions: z.array(
-        z.object({
-            question: z.string(),
-            intention: z.string(),
-            answer: z.string()
-        })
-    ),
+//     behavioralQuestions: z.array(
+//         z.object({
+//             question: z.string(),
+//             intention: z.string(),
+//             answer: z.string()
+//         })
+//     ),
 
-    skillGaps: z.array(
-        z.object({
-            skill: z.string(),
-            severity: z.enum(["low", "medium", "high"])
-        })
-    ),
+//     skillGaps: z.array(
+//         z.object({
+//             skill: z.string(),
+//             severity: z.enum(["low", "medium", "high"])
+//         })
+//     ),
 
-    preparationPlan: z.array(
-        z.object({
-            day: z.number(),
-            focus: z.string(),
-            tasks: z.array(z.string())
-        })
-    )
-});
+//     preparationPlan: z.array(
+//         z.object({
+//             day: z.number(),
+//             focus: z.string(),
+//             tasks: z.array(z.string())
+//         })
+//     )
+// });
 
-const generateInterviewReport = async ({
-    resume,
-    selfDescription,
-    jobDescription
-}) => {
+// const generateInterviewReport = async ({
+//     resume,
+//     selfDescription,
+//     jobDescription
+// }) => {
 
-    const prompt = `
+//     const prompt = `
+// You are a Senior Technical Interviewer.
+
+// Return ONLY valid JSON.
+
+// Do NOT use markdown.
+
+// The JSON MUST have EXACTLY this structure:
+
+// {
+//   "matchScore": ..,
+//   "title": "...",
+//   "technicalQuestions": [
+//     {
+//       "question": "...",
+//       "intention": "...",
+//       "answer": "..."
+//     }
+//   ],
+//   "behavioralQuestions": [
+//     {
+//       "question": "...",
+//       "intention": "...",
+//       "answer": "..."
+//     }
+//   ],
+//   "skillGaps": [
+//     {
+//       "skill": "...",
+//       "severity": "low"
+//     }
+//   ],
+//   "preparationPlan": [
+//     {
+//       "day": 1,
+//       "focus": "...",
+//       "tasks": [
+//         "...",
+//         "..."
+//       ]
+//     }
+//   ]
+// }
+
+// Generate:
+// - 10 technical questions
+// - 5 behavioral questions
+// - 3-5 skill gaps
+// - 7 preparation days
+
+// Resume:
+// ${resume}
+
+// Self Description:
+// ${selfDescription}
+
+// Job Description:
+// ${jobDescription}
+// `;
+
+//     const response = await ai.models.generateContent({
+//         model: "gemini-2.5-flash",
+//         contents: prompt,
+//         config: {
+//             responseMimeType: "application/json"
+//         }
+//     });
+
+//     console.log("RAW RESPONSE:");
+//     console.log(response.text);
+
+//     const parsed = JSON.parse(response.text);
+
+//     return interviewReportSchema.parse(parsed);
+// };
+
+// module.exports = { generateInterviewReport }
+
+
+import { Groq } from 'groq-sdk';
+
+const groq = new Groq();
+
+const prompt = `
 You are a Senior Technical Interviewer.
 
 Return ONLY valid JSON.
@@ -108,22 +191,24 @@ Job Description:
 ${jobDescription}
 `;
 
-    const response = await ai.models.generateContent({
-        model: "gemini-2.5-flash",
-        contents: prompt,
-        config: {
-            responseMimeType: "application/json"
-        }
-    });
+const chatCompletion = await groq.chat.completions.create({
+  "messages": [
+    {
+      "role": "user",
+      "content": prompt
+    }
+  ],
+  "model": "openai/gpt-oss-120b",
+  "temperature": 1,
+  "max_completion_tokens": 8192,
+  "top_p": 1,
+  "stream": true,
+  "reasoning_effort": "medium",
+  "stop": null
+});
 
-    console.log("RAW RESPONSE:");
-    console.log(response.text);
-
-    const parsed = JSON.parse(response.text);
-
-    return interviewReportSchema.parse(parsed);
-};
-
-module.exports = { generateInterviewReport }
+for await (const chunk of chatCompletion) {
+  process.stdout.write(chunk.choices[0]?.delta?.content || '');
+}
 
 
