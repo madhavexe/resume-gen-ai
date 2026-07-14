ea1867c195ba89226339abb710e2fde3577d5a5f
madhav <madhavjoshi6280@gmail.com>
2026-07-14 11:51:19 +0530

    gemini-2.5-flash-lite

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -109,8 +109,7 @@ ${jobDescription}
 `;
 
     const response = await ai.models.generateContent({
-        // model: "gemini-3-flash-preview",
-        model: "gemini-2.5-flash-preview",
+        model: "gemini-2.5-flash-lite",
         contents: prompt,
         config: {
             responseMimeType: "application/json"
